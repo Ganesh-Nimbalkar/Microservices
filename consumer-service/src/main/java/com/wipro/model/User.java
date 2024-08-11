fcbf2a79e31ea4d850a981ae2b5fe835bd8c4a8e
Ganesh Nimbalkar <ganeshnimbalkar40890@gmail.com>
2024-08-11 17:39:29 +0530

    Microservices

---
+++ consumer-service/src/main/java/com/wipro/model/User.java
@@ -0,0 +1,23 @@
+package com.wipro.model;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+	
+	private Integer userId;
+	private String username;
+	private String password;
+	private LocalDateTime loggedin;
+	
+	public User() {
+		
+	}
+
+}
